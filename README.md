2aa16f9c9ccc182decc1c7b9b55f7d290b1677c7
Erik Weilow <eweilow@digitalkoppar.se>
2017-02-28 12:44:14 +0100

    Create backend base

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # website-backend
 The backend for the website of ÆSIR
+
+Requires a file called `configuration.json` in the repository root with the following contents:
+```json
+{
+  "slack": {
+    "recruitment": {
+      "webhookURL": "slack webhook URL for posting to a recruitment channel"
+    }
+  }
+}
+```
---
+++ server.js
@@ -0,0 +1,35 @@
+const spdy = require("spdy");
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+
+const SlackPoster = require("./lib/SlackPoster");
+SlackPoster.configuration = require("./configuration.json").slack.recruitment;
+
+const app = express();
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/recruitment", function(req, res, next) {
+  SlackPoster.postInterestedUser(req.body.name, req.body.email, req.body.programOfStudy, req.body.yearsLeft, req.body.levelOfStudy)
+  .then(() => {
+    res.status(200);
+    res.write("ok");
+    res.end();
+  })
+  .catch((err) => next(err));
+});
+
+require("http").createServer(app).listen(8080);
+console.log("Server listening on port 8080");
+
+SlackPoster.postInterestedUser(
+  "test",
+  "test@kth.se",
+  "test",
+  "4",
+  "bachelor"
+)
+.then(data => console.log(data))
+.catch(err => console.error(err));
